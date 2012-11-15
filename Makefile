b5009f6c03d7b7766d5770a8378e30f19c116973
Dan Bradley <danb@cs.wisc.edu>
2012-11-15 16:00:38 -0600

    Migrated from hg repo

---
+++ Makefile
@@ -0,0 +1,4 @@
+
+all:
+	tar cvzf parrot.tgz parrot
+	tar cvzf cms_siteconf.tgz SITECONF
---
+++ test/test.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+die() {
+  echo 2>&1 "$@"
+  exit 1
+}
+
+if ! [ -x test.sh ]; then
+  echo "You must run this test from the directory containing test.h"
+  exit 1
+fi
+
+rm -rf var
+mkdir var || die "Failed to create test/var"
+
+TEST_DIR=`pwd`/var
+
+export glidein_config=$TEST_DIR/glidein_config
+sed < glidein_config > $glidein_config "s|TEST_DIR|$TEST_DIR|g" || die "Failed to create test/var/glidein_config"
+
+mkdir $TEST_DIR/parrot
+tar x -C $TEST_DIR/parrot -f `pwd`/../parrot.tgz || die "Failed to extract parrot"
+
+mkdir $TEST_DIR/cms_siteconf
+tar x -C $TEST_DIR/cms_siteconf -f `pwd`/../cms_siteconf.tgz || die "Failed to extract cms_siteconf"
+
+touch $TEST_DIR/condor_vars.lst
+
+../parrot_setup $TEST_DIR/glidein_config || die "parrot_setup failed"
+../parrot_cms_setup $TEST_DIR/glidein_config || die "parrot_cms_setup failed"
+
+
+mkdir -p $TEST_DIR/execute
+cd $TEST_DIR/execute
+
+export _CONDOR_SCRATCH_DIR=`pwd`
+export _CONDOR_SLOT=1
+export _CONDOR_JOB_AD=.jobad
+echo RequiresCVMFS=True > $_CONDOR_JOB_AD
+
+export CVMFS_OSG_APP=`grep -i "^CVMFS_OSG_APP " $glidein_config | awk '{print $2}'`
+export GLIDEIN_PARROT=`grep -i "^GLIDEIN_PARROT " $glidein_config | awk '{print $2}'`
+export GLIDEIN_PARROT_OPTIONS=`grep -i "^GLIDEIN_PARROT_OPTIONS " $glidein_config | awk '{$1=""; print $0}'`
+
+sh ../../../cvmfs_job_wrapper test -d $CVMFS_OSG_APP || die "cvmfs_job_wrapper failed to find $CVMFS_OSG_APP"
+
+sh ../../../cvmfs_job_wrapper sh -c "ls > /dev/null" || die "cvmfs_job_wrapper failed to write to /dev/null"
+
+sh ../../../cvmfs_job_wrapper sh -c "mkdir -p workdir/blah1/blah2 && touch workdir/blah1/file1 && rm -rf workdir" || die "cvmfs_job_wrapper failed to create/remove workdir"
+
+# the following makes use of the fact that /tmp is remapped to _CONDOR_SCRATCH_DIR/tmp
+
+sh ../../../cvmfs_job_wrapper cp $CVMFS_OSG_APP/cmssoft/cms/SITECONF/local/PhEDEx/storage.xml /tmp || die "cvmfs_job_wrapper failed"
+
+diff ../cms_siteconf/SITECONF/local/PhEDEx/storage.xml tmp/storage.xml || die "storage.xml copied from within parrot does not match expected"
+
+echo "Success"
+
